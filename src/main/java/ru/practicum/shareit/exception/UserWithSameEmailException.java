62fe40d5e7d84ec31ec11b31a1750ef993824305
samoylenko <samoylenko@lanit.ru>
2023-01-04 15:26:05 +0300

    sprint 13, ShareIt part 1, iteration 2, fix 2

--- src/main/java/ru/practicum/shareit/exception/UserWithSameEmailException.java
+++ src/main/java/ru/practicum/shareit/exception/UserWithSameEmailException.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.exception;
 
-public class UserWithSameEmailException extends Exception{
+public class UserWithSameEmailException extends Exception {
     public UserWithSameEmailException(String message) {
         super(message);
     }
